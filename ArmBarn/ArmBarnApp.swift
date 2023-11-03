8d661b5673459dd232510d33995a0c6557cd53a1
Nolan Thompson <thompsonn2@allegheny.edu>
2023-11-03 09:40:58 -0400

    Initial Commit

---
+++ ArmBarn/ArmBarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmBarnApp.swift
+//  ArmBarn
+//
+//  Created by Nolan Thompson on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ArmBarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
