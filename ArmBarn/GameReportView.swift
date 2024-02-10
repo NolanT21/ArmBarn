253eb928e5858e13fae08d861925066cd12a57e8
NolanT21 <nolant21@outlook.com>
2024-02-10 10:33:08 -0500

    Added Scrollview with Pitch Usage LineChart section

--- ArmBarn/GameReportView.swift
+++ ArmBarn/GameReportView.swift
@@ -26,219 +26,257 @@ struct GameReportView: View {
         .init(label: "Total Balls", value: 23)
     ]
     
-    var view_padding: CGFloat = 12
+    var view_padding: CGFloat = 10
     var view_crnr_radius: CGFloat = 12
     
     var body: some View {
-        VStack{
-            Text("Game Summary")
-                .font(.title)
-                .bold()
-                .frame(maxWidth: .infinity, alignment: .leading)
+        ScrollView{
+            VStack{
+                Text("Game Summary")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, view_padding)
+                    .padding(.top, view_padding)
+                
+                HStack{
+                    
+                    VStack{
+                        
+                        HStack{
+                            Text("Box Score")
+                                .font(.subheadline)
+                            Spacer()
+                        }
+                        
+                        Grid(){
+                            GridRow{
+                            Text("IP")
+                            Text("Pit")
+                            Text("BF")
+                            Text("H")
+                            Text("SO")
+                            Text("BB")
+                            }
+                            .padding(.vertical, -5)
+                            .bold()
+                            
+                            Divider()
+                            
+                            GridRow{
+                            Text("9.0")
+                            Text("72")
+                            Text("20")
+                            Text("4")
+                            Text("7")
+                            Text("2")
+                            }
+                            
+                        }
+                        
+                    }
+                    .padding(view_padding)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
+                    
+                }
                 .padding(.horizontal, view_padding)
-            
-            HStack{
-                
-                VStack{
-                    
-                    HStack{
-                        Text("Box Score")
-                            .font(.subheadline)
-                        Spacer()
-                    }
-                    
-                    Grid(){
-                        GridRow{
-                        Text("IP")
-                        Text("Pit")
-                        Text("BF")
-                        Text("H")
-                        Text("SO")
-                        Text("BB")
-                        }
-                        .padding(.vertical, -5)
-                        .bold()
-                        
-                        Divider()
-                        
-                        GridRow{
-                        Text("9.0")
-                        Text("72")
-                        Text("20")
-                        Text("4")
-                        Text("7")
-                        Text("2")
-                        }
-                        
-                    }
-                    
-                }
-                .padding(view_padding)
-                .frame(maxWidth: .infinity)
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
-                
-            }
-            .padding(view_padding)
-            
-            HStack{
-                
-                VStack{
-                    HStack{
-                        Text("1st Pitch Strike %")
-                            .font(.subheadline)
-                            .padding(.top, view_padding)
-                        
-                        Spacer()
-                    }
-                    .padding(.leading, view_padding)
-                    
-                    VStack(alignment: .trailing){
-                        Chart(firstpitstrike_data) { fps in
-                            SectorMark(
-                                angle: .value(
-                                    Text(verbatim: fps.label),
-                                    fps.value
-                                ),
-                                innerRadius: .ratio(0.7)
-                            )
-                            .foregroundStyle(
-                                by: .value(
-                                    Text(verbatim: fps.label),
-                                    fps.label
-                                )
-                            )
-                        }
-                        .frame(width: 160, height: 140)
-                        .chartLegend(.hidden)
-                        .chartBackground { proxy in
-                            VStack{
-                                Text("59%")
-                                    .font(.system(size: 33))
-                                    .bold()
-                                Text("13/22")
-                                    .font(.system(size: 13))
-                            }
-                            .padding(.top, view_padding)
-                        }
-                        
-                    }
-                    .padding(.bottom, view_padding)
-                    //.padding(.leading, view_padding)
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
                 .padding(.bottom, view_padding)
-                .padding(.leading, view_padding)
-                .padding(.trailing, view_padding/2)
                 
-                VStack{
-                    HStack{
-                        Text("Strike %")
-                            .font(.subheadline)
-                            .padding(.top, view_padding)
+                HStack{
+                    
+                    VStack{
+                        HStack{
+                            Text("1st Pitch Strike %")
+                                .font(.subheadline)
+                                .padding(.top, view_padding)
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, view_padding)
                         
-                        Spacer()
-                    }
-                    .padding(.leading, view_padding)
-                   
-                    VStack(alignment: .trailing){
-                        
-                        Chart(strikepercent_data) { sp in
-                            SectorMark(
-                                angle: .value(
-                                    Text(verbatim: sp.label),
-                                    sp.value
-                                ),
-                                innerRadius: .ratio(0.7)
-                            )
-                            .foregroundStyle(
-                                by: .value(
-                                    Text(verbatim: sp.label),
-                                    sp.label
+                        VStack(alignment: .trailing){
+                            Chart(firstpitstrike_data) { fps in
+                                SectorMark(
+                                    angle: .value(
+                                        Text(verbatim: fps.label),
+                                        fps.value
+                                    ),
+                                    innerRadius: .ratio(0.7)
                                 )
-                            )
-                        }
-                        .frame(width: 160, height: 140)
-                        .chartLegend(.hidden)
-                        .chartBackground { proxy in
-                            VStack{
-                                Text("67%")
-                                    .font(.system(size: 33))
-                                    .bold()
-                                Text("45/68")
-                                    .font(.system(size: 13 ))
+                                .foregroundStyle(
+                                    by: .value(
+                                        Text(verbatim: fps.label),
+                                        fps.label
+                                    )
+                                )
                             }
-                            .padding(.top, view_padding)
+                            .frame(width: 160, height: 140)
+                            .chartLegend(.hidden)
+                            .chartBackground { proxy in
+                                VStack{
+                                    Text("59%")
+                                        .font(.system(size: 33))
+                                        .bold()
+                                    Text("13/22")
+                                        .font(.system(size: 13))
+                                }
+                                .padding(.top, view_padding)
+                            }
+                            
                         }
+                        .padding(.bottom, view_padding)
                         
                     }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
                     .padding(.bottom, view_padding)
+                    .padding(.leading, view_padding)
+                    .padding(.trailing, view_padding/2)
+                    
+                    VStack{
+                        HStack{
+                            Text("Strike %")
+                                .font(.subheadline)
+                                .padding(.top, view_padding)
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, view_padding)
+                       
+                        VStack(alignment: .trailing){
+                            
+                            Chart(strikepercent_data) { sp in
+                                SectorMark(
+                                    angle: .value(
+                                        Text(verbatim: sp.label),
+                                        sp.value
+                                    ),
+                                    innerRadius: .ratio(0.7)
+                                )
+                                .foregroundStyle(
+                                    by: .value(
+                                        Text(verbatim: sp.label),
+                                        sp.label
+                                    )
+                                )
+                            }
+                            .frame(width: 160, height: 140)
+                            .chartLegend(.hidden)
+                            .chartBackground { proxy in
+                                VStack{
+                                    Text("67%")
+                                        .font(.system(size: 33))
+                                        .bold()
+                                    Text("45/68")
+                                        .font(.system(size: 13 ))
+                                }
+                                .padding(.top, view_padding)
+                            }
+                            
+                        }
+                        .padding(.bottom, view_padding)
 
-                   
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
-                .padding(.bottom, view_padding)
-                .padding(.leading, view_padding/2)
-                .padding(.trailing, view_padding)
-                
-                
-            }
-            
-            HStack{
-                
-                Spacer()
-                    .frame(width:view_padding)
-                
-                VStack{
-                    Text("Game Score")
-                        .font(.subheadline)
-                        .padding(view_padding)
-                    Text("")
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
-                
-                Spacer()
-                    .frame(width:view_padding)
-                
-            }
-            
-            Spacer()
-            
-            HStack{
-                VStack{
-                    HStack{
-                        Text("Pitch Location")
-                            .font(.subheadline)
-                            .padding(view_padding)
+                       
                     }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
+                    .padding(.bottom, view_padding)
+                    .padding(.leading, view_padding/2)
+                    .padding(.trailing, view_padding)
                     
-                    Image("PLO_Background")
-                        .resizable()
-                        //.gesture(tap)
-                        .aspectRatio(contentMode: .fit)
                     
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
-                .padding(.bottom, view_padding)
-                .padding(.leading, view_padding/2)
-                .padding(.trailing, view_padding)
+                
+                HStack{
+                    VStack{
+                        HStack{
+                            Text("Game Score")
+                                .font(.subheadline)
+                                .padding(.leading, view_padding)
+                                .padding(.top, view_padding)
+                            
+                            Spacer()
+                            
+                        }
+                        
+                        Text("")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
+                    .padding(.bottom, view_padding)
+                    .padding(.leading, view_padding)
+                    .padding(.trailing, view_padding)
+                    
+                }
+                
+                Spacer()
+                
+                HStack{
+                    VStack{
+                        HStack{
+                            Text("Pitch Location")
+                                .font(.subheadline)
+                                .padding(.leading, view_padding)
+                                .padding(.top, view_padding)
+                            Spacer()
+                        }
+                        
+                        Image("PLO_Background")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                            .padding(.horizontal, 6)
+                            .padding(.bottom, 6)
+                            //.gesture(tap)
+                            
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
+                    .padding(.bottom, view_padding)
+                    .padding(.leading, view_padding)
+                    .padding(.trailing, view_padding)
+                }
+                
+                Spacer()
+                
+                HStack{
+                    VStack{
+                        HStack{
+                            Text("Pitch Usage by Inning")
+                                .font(.subheadline)
+                                .padding(.leading, view_padding)
+                                .padding(.top, view_padding)
+                            
+                            Spacer()
+                        }
+                        
+                        
+                        
+                        Text("")
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: view_crnr_radius))
+                    .padding(.bottom, view_padding)
+                    .padding(.leading, view_padding)
+                    .padding(.trailing, view_padding)
+                }
+                
+                Spacer()
+                
             }
-            
-            Spacer()
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.gray)
-        
+        .navigationTitle("Game Summary")
     }
 }
 
