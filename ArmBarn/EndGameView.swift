4c1cd8201cdddffb362442589577ceea4d1f7b47
NolanT21 <nolant21@outlook.com>
2024-03-16 13:48:02 -0400

    Fixed Hitlog Crash by clearing report variables on new game

--- ArmBarn/EndGameView.swift
+++ ArmBarn/EndGameView.swift
@@ -63,6 +63,9 @@ struct EndGameView: View {
         
     }
     func new_game_func() {
+        
+        clear_game_report()
+        
         scoreboard.balls = 0
         scoreboard.strikes = 0
         scoreboard.outs = 0
@@ -87,6 +90,42 @@ struct EndGameView: View {
         scoreboard.o1light = .black
         scoreboard.o2light = .black
         
+    }
+    
+    func clear_game_report() {
+        game_report.batters_faced = 0
+        game_report.strikes = 0
+        game_report.balls = 0
+        game_report.hits = 0
+        game_report.strikeouts = 0
+        game_report.walks = 0
+        
+        game_report.first_pitch_strike = 0
+        game_report.first_pitch_ball = 0
+        game_report.first_pit_strike_per = 0
+        game_report.fpb_to_fps = []
+        
+        game_report.strikes_per = 0
+        game_report.balls_to_strikes = []
+        
+        game_report.game_score = 40
+        game_report.pitches = scoreboard.pitches
+        
+        game_report.p1_by_inn = [0]
+        game_report.p2_by_inn = [0]
+        game_report.p3_by_inn = [0]
+        game_report.p4_by_inn = [0]
+        
+        game_report.x_coordinate_list = []
+        game_report.y_coordinate_list = []
+        game_report.pl_color_list = []
+        game_report.pl_outline_list = []
+        game_report.outs_hitlog = []
+        
+        game_report.inn_hitlog = []
+        game_report.result_hitlog = []
+        game_report.pitchtype_hitlog = []
+        game_report.cnt_hitlog = []
     }
 }
 
