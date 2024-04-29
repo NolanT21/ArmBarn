1b6bddf075f13ae467c969a8250dbcca15b210d9
NolanT21 <nolant21@outlook.com>
2024-04-28 21:59:13 -0400

    Added homeplate to batter stance popup view

--- BatterPositionView.swift
+++ BatterPositionView.swift
@@ -14,11 +14,14 @@ struct BatterPositionView: View {
     
     @State private var font_color: Color = .white
     @State private var offset: CGFloat = 1000
-    
-    var crnr_radius: CGFloat = 12
+    @State private var crnr_radius: CGFloat = 12
     
     @Binding var isActive: Bool
     var close_action: () -> ()
+    
+    //@Bindable var cur_ab_events: Event
+    
+//    @Binding var cur_ab_events: [Event]
 
     var body: some View {
         ZStack{
@@ -31,61 +34,99 @@ struct BatterPositionView: View {
                     .font(.system(size: 22, weight: .bold))
                     .foregroundStyle(font_color)
                     .padding()
-                Grid() {
-                    GridRow{
-                        Image(systemName: "figure.baseball")
-                            .imageScale(.large)
-                            .font(.system(size: 60))
-                            .foregroundColor(.white)
-                            .bold()
-                        
-                        Image(systemName: "figure.baseball")
-                            .scaleEffect(x: -1, y: 1)
-                            .imageScale(.large)
-                            .font(.system(size: 60))
-                            .foregroundColor(.white)
-                            .bold()
-                    }
-                    GridRow{
-                        Button {
-                            event.batter_stance = "R"
-                            close()
-                            close_action()
-                        } label: {
-                            ZStack{
-                                RoundedRectangle(cornerRadius: crnr_radius)
-                                    .foregroundColor(Color("ScoreboardGreen"))
-                                
-                                Text("RIGHT")
-                                    .font(.system(size: 20, weight: .bold))
-                                    .foregroundStyle(font_color)
-                                    .padding()
+                ZStack{
+                    Grid() {
+                        GridRow{
+                            Image(systemName: "figure.baseball")
+                                .rotation3DEffect(.degrees(20), axis: (x: 0, y: 1, z: 0))
+                                //.scaleEffect(x: 0.9, y: 1)
+                                .imageScale(.large)
+                                .font(.system(size: 60))
+                                .foregroundColor(.white)
+                                .bold()
+                            
+                            Image(systemName: "figure.baseball")
+                                .scaleEffect(x: -1, y: 1)
+                                .rotation3DEffect(.degrees(-20), axis: (x: 0, y: 1, z: 0))
+                                .imageScale(.large)
+                                .font(.system(size: 60))
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                        GridRow{
+                            Button {
+                                event.batter_stance = "R"
+                                close()
+                                close_action()
+                            } label: {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: crnr_radius)
+                                        .foregroundColor(Color("ScoreboardGreen"))
+                                    
+                                    Text("RIGHT")
+                                        .font(.system(size: 20, weight: .bold))
+                                        .foregroundStyle(font_color)
+                                        .padding()
+                                }
+                            }
+                            
+                            Button {
+                                event.batter_stance = "L"
+                                close()
+                                close_action()
+                            } label: {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: crnr_radius)
+                                        .foregroundColor(Color("ScoreboardGreen"))
+                                    
+                                    Text("LEFT")
+                                        .font(.system(size: 20, weight: .bold))
+                                        .foregroundStyle(font_color)
+                                        .padding()
+                                }
                             }
                         }
-                        
-                        Button {
-                            event.batter_stance = "L"
-                            close()
-                            close_action()
-                        } label: {
-                            ZStack{
-                                RoundedRectangle(cornerRadius: crnr_radius)
-                                    .foregroundColor(Color("ScoreboardGreen"))
-                                
-                                Text("LEFT")
-                                    .font(.system(size: 20, weight: .bold))
-                                    .foregroundStyle(font_color)
-                                    .padding()
-                            }
-                        }
                     }
+                    
+                    VStack{
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            Image("HomeplateFromBehind")
+                                .resizable()
+                                .frame(width: 50.0, height: 25.0)
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                    
                 }
-                
             }
             .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
             .padding()
             .background(Color("DarkGrey"))
             .clipShape(RoundedRectangle(cornerRadius: crnr_radius))
+            .overlay{
+                VStack{
+                    
+                    HStack{
+                        
+                        Spacer()
+                        
+                        Button{
+                            close()
+                            close_action()
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.white)
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                }
+                .padding()
+            }
             .shadow(radius: 20)
             .padding(30)
             .offset(x: 0, y: offset)
@@ -96,7 +137,7 @@ struct BatterPositionView: View {
             }
             
         }
-        .padding(.top, 50)
+        .padding(.top, 45)
         .ignoresSafeArea()
     }
     
@@ -108,6 +149,25 @@ struct BatterPositionView: View {
     
 }
 
-#Preview {
-    BatterPositionView(isActive: .constant(true), close_action: {})
-}
+//#Preview {
+//    BatterPositionView(isActive: .constant(true), close_action: {})
+//}
+
+//if events.count > 0 {
+//    if (scoreboard.balls != 0 || scoreboard.strikes != 0) && events[events.count - 1].result_detail != "R"{
+//        let cur_ab = events[events.count - 1]
+//        var prev_ab_index = events.count - 2
+//        var prev_ab = events[prev_ab_index]
+//        while cur_ab.atbats == prev_ab.atbats {
+//            
+//            cur_ab_events.append(prev_ab)
+//            
+//            if prev_ab_index > 0 {
+//                prev_ab_index -= 1
+//            }
+//            else {
+//                break
+//            }
+//        }
+//    }
+//}
