f1354f92691594894b213a02452a81aef0a3aff0
NolanT21 <nolant21@outlook.com>
2024-05-05 22:01:09 -0400

    Added variables for hit summary and pbp log

--- ArmBarn/report_variables.swift
+++ ArmBarn/report_variables.swift
@@ -39,11 +39,49 @@ import Observation
     var pl_color_list: [Color] = []
     var pl_outline_list: [Color] = []
     
+    var singles: Int = 0
+    var doubles: Int = 0
+    var triples: Int = 0
+    var homeruns: Int = 0
+    var errors: Int = 0
+    var p1_hits: Int = 0
+    var p2_hits: Int = 0
+    var p3_hits: Int = 0
+    var p4_hits: Int = 0
+    var most_hit_pit: String = ""
+    var mhp_pitches: Int = 0
+    var mhp_hits: Int = 0
+    
+    var swings: Int = 0
+    var swing_per: Int = 0
+    var whiffs: Int = 0
+    var whiff_per: Int = 0
+    
     var p1_by_inn: [Int] = []
     var p2_by_inn: [Int] = []
     var p3_by_inn: [Int] = []
     var p4_by_inn: [Int] = []
     var pitches_by_inn: [PitchTypeDataset] = []
+    
+    var pbp_event_list: [PBPLog] = []
+    var cur_pitcher_id = UUID()
+    
+}
+
+struct PBPLog: Identifiable {
+    let id = UUID()
+    var pitch_num : Int
+    var pitch_type : String
+    var result : String
+    var balls : Int
+    var strikes : Int
+    var outs : Int
+    var out_label : String
+    var velo : Double
+    var inning : Int
+    
+    var result_detail : String
+    var pitcher : String
     
 }
 
