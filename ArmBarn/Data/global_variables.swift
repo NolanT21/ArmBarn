caeb6253a9fce223c569c5634137f86cd1b5a47a
NolanT21 <nolant21@outlook.com>
2025-01-03 18:25:01 -0500

    Folder Restructuring

---
+++ ArmBarn/Data/global_variables.swift
@@ -0,0 +1,175 @@
+//
+//  scoreboard.swift
+//  ArmBarn
+//
+//  Created by Nolan Thompson on 11/18/23.
+//
+
+import SwiftUI
+import SwiftData
+import Observation
+
+@Observable class Scoreboard{
+    var balls: Int = 0
+    var strikes: Int = 0
+    var outs: Int = 0
+    var pitches: Int = 0
+    var atbats: Int = 1
+    var inning: Int = 1
+    
+    var baserunners: Int = 0
+    
+    var b1light: Bool = false
+    var b2light: Bool = false
+    var b3light: Bool = false
+    
+    var s1light: Bool = false
+    var s2light: Bool = false
+    
+    var o1light: Bool = false
+    var o2light: Bool = false
+    
+    var update_scoreboard: Bool = true
+    var enable_bottom_row: Bool = true
+    
+    var pitchers_appearance_list: [PitchersAppeared] = []
+}
+
+struct PitchersAppeared {
+    var pitcher_id: UUID
+    var pitches: Int
+    var batters_faced: Int
+}
+
+@Observable class Event_String{
+    var pitch_result: String = "NONE"
+    var pitch_type: String = "NP"
+    var result_detail: String = ""
+    var balls: Int = 0
+    var strikes: Int = 0
+    var outs: Int = 0
+    var inning: Int = 1
+    var atbats: Int = 1
+    var x_cor: Double = 0
+    var y_cor: Double = 0
+    var batter_stance: String = ""
+    var velocity: Double = 0
+    var event_number: Int = 0
+    
+    var end_ab_rd: [String] = ["S", "D", "T", "H", "E", "B", "F", "G", "L", "P", "Y", "W", "K", "C", "M", "RE"]
+    var newAtBat: Bool = false
+    
+    var recordEvent: Bool = false
+}
+
+@Observable class currentPitcher{
+    var idcode = UUID()
+    var firstName: String = ""
+    var lastName: String = "Change Me"
+    var pitch1: String = "None"
+    var pitch2: String = "None"
+    var pitch3: String = "None"
+    var pitch4: String = "None"
+    var arsenal: [String] = ["None", "None", "None", "None"]
+    var pitch_num: Int = 0
+}
+
+@Observable class PitchTypeConfig{
+    var ptcolor: Color = .pink
+    var ab_pitch_color: [Color] = []
+    var pitch_x_loc: [CGFloat] = []
+    var pitch_y_loc: [CGFloat] = []
+    var pitch_cur_ab: Int = 0
+    var arsenal_colors: [Color] = [Color("PowderBlue"), Color("Gold"), Color("Tangerine"), Color("Grey")]
+    var hidePitchOverlay: Bool = false
+    var loadPitchOverlay: Bool = false
+    var non_pitch_event = false
+    var npe_EOAB = false
+}
+
+@Observable class BullpenConfig{
+    var expected_target: String = ""
+    var actual_target: String = ""
+    var pitch_type: String = ""
+    var pp_number: Int = 0
+    var pp_pitchtype: String = "-"
+    var pp_spot_detail: Image = Image(systemName: "minus")
+    var pp_spot_color: Color = .white
+    var event_number: Int = 0
+}
+
+@Model class BullpenEvent{
+    var pitcher_id = UUID()
+    var expected_target: String = ""
+    var actual_target: String = ""
+    var pitch_type: String = ""
+    var event_number: Int = 0
+    
+    init(pitcher_id: UUID, expected_target: String, actual_target: String, pitch_type: String, event_number: Int) {
+        self.pitcher_id = pitcher_id
+        self.expected_target = expected_target
+        self.actual_target = actual_target
+        self.pitch_type = pitch_type
+        self.event_number = event_number
+    }
+    
+}
+
+@Model class Event{
+    
+    @Attribute(.externalStorage) var pitcher_id = UUID()
+    
+    @Attribute(.externalStorage) var pitch_result: String
+    @Attribute(.externalStorage) var pitch_type: String
+    @Attribute(.externalStorage) var result_detail: String
+    @Attribute(.externalStorage) var balls: Int
+    @Attribute(.externalStorage) var strikes: Int
+    @Attribute(.externalStorage) var outs: Int
+    @Attribute(.externalStorage) var inning: Int
+    @Attribute(.externalStorage) var atbats: Int
+    @Attribute(.externalStorage) var pitch_x_location: Double = 0
+    @Attribute(.externalStorage) var pitch_y_location: Double = 0
+    @Attribute(.externalStorage) var batter_stance: String = ""
+    @Attribute(.externalStorage) var velocity: Double = 0
+    @Attribute(.externalStorage) var event_number: Int = 0
+    
+    init(pitcher_id: UUID, pitch_result: String, pitch_type: String, result_detail: String, balls: Int, strikes: Int, outs: Int, inning: Int, atbats: Int, pitch_x_location: Double, pitch_y_location: Double, batter_stance: String, velocity: Double, event_number: Int) {
+        self.pitcher_id = pitcher_id
+        self.pitch_result = pitch_result
+        self.pitch_type = pitch_type
+        self.result_detail = result_detail
+        self.balls = balls
+        self.strikes = strikes
+        self.outs = outs
+        self.inning = inning
+        self.atbats = atbats
+        self.pitch_x_location = pitch_x_location
+        self.pitch_y_location = pitch_y_location
+        self.batter_stance = batter_stance
+        self.velocity = velocity
+        self.event_number = event_number
+    }
+}
+
+@Model class Pitcher{
+    var id = UUID()
+    var firstName: String
+    var lastName: String
+    var pitch1: String
+    var pitch2: String
+    var pitch3: String
+    var pitch4: String
+    
+    init(id: UUID, firstName: String, lastName: String, pitch1: String, pitch2: String, pitch3: String, pitch4: String){
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.pitch1 = pitch1
+        self.pitch2 = pitch2
+        self.pitch3 = pitch3
+        self.pitch4 = pitch4
+    }
+}
+
+
+
---
+++ ArmBarn/Data/report_variables.swift
@@ -0,0 +1,202 @@
+//
+//  report_variables.swift
+//  ArmBarn
+//
+//  Created by Nolan Thompson on 2/13/24.
+//
+
+import SwiftUI
+import SwiftData
+import Observation
+
+@Observable class GameReport{
+    var inn_pitched: Double = 0.0
+    var pitches: Int = 0
+    var batters_faced: Int = 0
+    var hits: Int = 0
+    var strikeouts: Int = 0
+    var walks: Int = 0
+    
+    var first_pitch_strike: Int = 0
+    var first_pitch_ball: Int = 0
+    var first_pit_strike_per: Int = 0
+    var fpb_to_fps: [Int] = [0, 0]
+
+    var strikes: Int = 0
+    var balls: Int = 0
+    var strikes_per: Int = 0
+    var balls_to_strikes: [Int] = [0, 0]
+    
+    var game_score: Int = 40
+    var game_score_min: Int = 0
+    var game_score_max: Int = 0
+    
+    var x_coordinate_list: [Double] = []
+    var y_coordinate_list: [Double] = []
+    var pl_color: Color = .clear
+    var pl_outline: Color = .clear
+    var pl_color_list: [Color] = []
+    var pl_outline_list: [Color] = []
+    
+    var singles: Int = 0
+    var doubles: Int = 0
+    var triples: Int = 0
+    var homeruns: Int = 0
+    var errors: Int = 0
+    var p1_hits: Int = 0
+    var p2_hits: Int = 0
+    var p3_hits: Int = 0
+    var p4_hits: Int = 0
+    var most_hit_pit: String = ""
+    var mhp_pitches: Int = 0
+    var mhp_hits: Int = 0
+    
+    var swings: Int = 0
+    var swing_per: Int = 0
+    var whiffs: Int = 0
+    var whiff_per: Int = 0
+    
+    var p1_velo_list: [Double] = []
+    var p2_velo_list: [Double] = []
+    var p3_velo_list: [Double] = []
+    var p4_velo_list: [Double] = []
+    
+    var velo_set_list: [PitchVeloSet] = []
+    
+    var rh_batters_faced: Int = 0
+    var lh_batters_faced: Int = 0
+    var bs_faced_factor: Double = 0
+    var rh_hits: Int = 0
+    var lh_hits: Int = 0
+    var bs_hits_factor: Double = 0
+    var rh_xbhs: Int = 0
+    var lh_xbhs: Int = 0
+    var bs_xbhs_factor: Double = 0
+    var rh_strikeouts: Int = 0
+    var lh_strikeouts: Int = 0
+    var bs_strikeouts_factor: Double = 0
+    var rh_walks: Int = 0
+    var lh_walks: Int = 0
+    var bs_walks_factor: Double = 0
+    
+    var p1_by_inn: [Int] = []
+    var p2_by_inn: [Int] = []
+    var p3_by_inn: [Int] = []
+    var p4_by_inn: [Int] = []
+    var pitches_by_inn: [PitchTypeDataset] = []
+    
+    var pbp_event_list: [PBPLog] = []
+    var cur_pitcher_id = UUID()
+    
+    var game_location: String = ""
+    var opponent_name: String = ""
+    
+}
+
+@Observable class AtBatBreakdown {
+    
+    var at_bat_list: [AtBatDT] = []
+        
+}
+
+struct AtBatDT {
+    //var result: String = ""
+    var outs: Int = 0
+    var inning: Int = 0
+    var batter_hand: String = ""
+    var pitch_list: [pitch_info_ab] = []
+    var x_coor_list: [Double] = []
+    var y_coor_list: [Double] = []
+    var pitch_color_list: [Color] = []
+    var plot_pitch_list: [Int] = []
+    var pitch_num_list: [Int] = []
+}
+
+struct pitch_info_ab {
+    var result: String = ""
+    var result_color: Color
+    var pitch_type: String = ""
+    var velocity: Double = 0
+    var balls: Int = 0
+    var strikes: Int = 0
+    var units: String = ""
+}
+
+struct PBPLog{
+    //let id = UUID()
+    var pitch_num : Int
+    var pitch_type : String
+    var result : String
+    var balls : Int
+    var strikes : Int
+    var outs : Int
+    var out_label : String
+    var velo : Double
+    var inning : Int
+    
+    var result_detail : String
+    var pitcher : String
+    
+}
+
+struct PitchVeloSet: Identifiable {
+    let id = UUID()
+    var pitch_type : String
+    var max_velo : Double
+    var avg_velo : Double
+    var velo_factor: Double
+}
+
+struct PitchTypeDataset: Identifiable {
+    let id = UUID()
+    let name: String
+    let dataset: [Int]
+}
+
+
+@Observable class BullpenReport{
+    var pitches: Int = 0
+    var spots_hit: Int = 0
+    
+    var p1_pitches: Int = 0
+    var p1_spots_hit: Int = 0
+    var p2_pitches: Int = 0
+    var p2_spots_hit: Int = 0
+    var p3_pitches: Int = 0
+    var p3_spots_hit: Int = 0
+    var p4_pitches: Int = 0
+    var p4_spots_hit: Int = 0
+    
+    var spots_by_pitch_list: [SpotsByPitchType] = []
+    
+    var bp_pbp_list: [BP_PBPLog] = []
+}
+
+struct SpotsByPitchType: Identifiable {
+    let id = UUID()
+    var pitch_type: String
+    var pitch_num: Int
+    var spots_hit: Int
+}
+
+struct BP_PBPLog: Identifiable {
+    let id = UUID()
+    var pitch_num: Int
+    var pitch_type: String
+    var expected_spot: String
+    var actual_spot: String
+}
+
+//Pitch Type Amount by Inning Line Chart Data Structure
+struct Inning: Identifiable {
+    let id = UUID()
+    let inn_number: Int
+    let amount: Int
+}
+
+struct PitchType: Identifiable {
+    let id = UUID()
+    let name: String
+    let pitchtype_data: [Inning]
+}
+
