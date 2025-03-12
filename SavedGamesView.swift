bd5ea6eeea89c47dd48e6d364f9edabe35729c93
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-03-11 22:26:44 -0400

    Miscellaneous Changes

---
+++ SavedGamesView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedGamesView.swift
+//  ArmBarn
+//
+//  Created by Nolan Thompson on 2/7/25.
+//
+
+import SwiftUI
+
+struct SavedGamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedGamesView()
+}
