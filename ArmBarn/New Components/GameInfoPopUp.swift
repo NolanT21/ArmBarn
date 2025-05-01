b017505c686e24370e9a78ae57151a46d54786d8
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-05-01 18:03:52 -0400

    Added Game Information Input PopUp

---
+++ ArmBarn/New Components/GameInfoPopUp.swift
@@ -0,0 +1,186 @@
+//
+//  GameInfoPopUp.swift
+//  ArmBarn
+//
+//  Created by Nolan Thompson on 4/29/25.
+//
+
+import SwiftUI
+
+struct GameInfoPopUp: View {
+    
+    @Environment(GameReport.self) var game_report
+    @Environment(LocationOverlay.self) var location_overlay
+    
+    @State private var validTeamName: Bool = false
+    
+    @State private var opponentname: String = String()
+    @FocusState private var fieldIsFocused: Bool
+    
+    @State var game_location: [String] = ["Home", "Away"]
+    @State private var selected_location = "Home"
+    
+    @State private var start_date = Date.now
+    
+    @State var close_action: () -> ()
+    
+    var body: some View {
+        ZStack{
+            Color.black.opacity(0.5)
+            
+            VStack{
+                
+                Spacer()
+                    .frame(height: 100)
+                    
+                    VStack(){
+                        
+                        Spacer()
+                            .frame(maxHeight: 10)
+                        
+                        VStack(spacing: 20){
+                            Text("Game Information")
+                                .font(.system(size: 17, weight: .semibold))
+                                                        
+                            TextField("Enter Opponent Name", text: $opponentname)
+                                .padding(.vertical, 5)
+                                .padding(.leading, 10)
+                                .focused($fieldIsFocused)
+                                .submitLabel(.done)
+                                //.onSubmit {
+                                    //event.velocity = veloinput
+                                    //close()
+                                    //close_action()
+                                //}
+                                .font(.system(size: 17, weight: .medium))
+                                .background(Color.gray.opacity(0.2))
+                                .background(.regularMaterial)
+                                .cornerRadius(8)
+                                .multilineTextAlignment(.leading)
+                                .padding(.horizontal, 20)
+                                .onAppear {
+                                    fieldIsFocused = true
+                                }
+                            
+                            Picker("", selection: $selected_location) {
+                                ForEach(game_location, id: \.self) {
+                                    Text($0)
+                                        .font(.system(size: 13, weight: .semibold))
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+                            .padding(.horizontal, 20)
+                            .onChange(of: selected_location){
+                                //impact.impactOccurred()
+                                //game_report.game_location = selected_location
+                                //ASGameLocation = selected_location
+                            }
+                            
+                            VStack{
+                                
+                                Text("Start Time")
+                                    .font(.system(size: 15, weight: .semibold))
+                                    .foregroundStyle(Color.white)
+                                HStack{
+                                    Spacer()
+                                    
+                                    DatePicker("", selection: $start_date)
+                                        .labelsHidden()
+                                        .transformEffect(.init(scaleX: 0.9, y: 0.9))
+                                        .accentColor(Color("ScoreboardGreen"))
+                                        .padding(.horizontal, 20)
+                                        .padding(.top, -5)
+                                        .padding(.leading, 25)
+                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+
+                                    
+                                    Spacer()
+                                }
+                            }
+                            
+                            Button {
+                                //Logic for storing game information
+                                game_report.opponent_name = opponentname
+                                game_report.game_location = selected_location
+                                game_report.start_date = start_date
+                                
+                                fieldIsFocused = false
+                                withAnimation{
+                                    close_action()
+                                    location_overlay.game_info_entered = true
+                                }
+                            } label: {
+                                Text("Enter")
+                                    .font(.system(size: 17, weight: .bold))
+                                    .frame(width: 125, height: 40)
+                                    .background(!validTeamName || opponentname == ""  ? Color.gray.opacity(0.5) : Color("ScoreboardGreen"))
+                                    .foregroundColor(!validTeamName || opponentname == "" ? Color.gray.opacity(0.5) : Color.white)
+                                    .cornerRadius(8.0)
+                            }
+                            .disabled(!validTeamName || opponentname == "")
+                            .onChange(of: opponentname){ _, _ in
+                                validate_opponent_name()
+                            }
+                        }
+                        .overlay{
+                            VStack{
+                                
+                                HStack{
+                                    Button {
+                                        fieldIsFocused = false
+                                        withAnimation{
+                                            close_action()
+                                        }
+                                    } label: {
+                                        Circle()
+                                            .fill(Color.gray.opacity(0.3))
+                                            .frame(width: 20, height: 20)
+                                            .overlay{
+                                                Image(systemName: "xmark")
+                                                    .foregroundColor(.white)
+                                                    .font(.system(size: 11).bold())
+                                            }
+                                    }
+                                    .padding(.top, -10)
+                                    .padding(.horizontal, -3)
+                                    
+                                    Spacer()
+                                    
+                                }
+                                
+                                Spacer()
+                            }
+                        }
+                        
+                        Spacer()
+                            .frame(maxHeight: 10)
+                    
+                }
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color.black.opacity(0.2))
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 20)
+                .padding(45)
+                
+                Spacer()
+                
+            }
+        }
+        .onAppear{
+            if location_overlay.game_info_entered == true {
+                opponentname = game_report.opponent_name
+                selected_location = game_report.game_location
+                start_date = game_report.start_date
+            }
+        }
+    }
+    
+    func validate_opponent_name() {
+        let validate = NSPredicate(format: "SELF MATCHES %@", "^[a-zA-Z\\s\\&\\-]*$")
+        validTeamName = validate.evaluate(with: opponentname)
+    }
+    
+}
