af879c10a2e70af5f4484fed15505fa4087a35b9
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-07-09 10:23:48 -0400

    Removed re-entry logic, adjusted logic for adding pitcher to appearance list

--- ArmBarn/Shared/SelectPitcherView.swift
+++ ArmBarn/Shared/SelectPitcherView.swift
@@ -70,43 +70,56 @@ struct SelectPitcherView: View {
                                     
                                     Button{
                                         
-                                        pitcher_select_haptic.toggle()
+                                        //Conditional Logic for calling pitcher re-entry popup
+                                        //if !scoreboard.pitchers_appearance_list.contains(where: {$0.pitcher_id == p_er.id}) {
+                                            
+                                            pitcher_select_haptic.toggle()
+                                            
+                                            if p_er.id != current_pitcher.idcode {
+                                                store_pitcher_appearance()
+                                                current_pitcher.pitch_num = 0
+                                                current_pitcher.firstName = p_er.firstName
+                                                current_pitcher.lastName = p_er.lastName
+                                                current_pitcher.idcode = p_er.id
+                                                selected_pitcher_id = p_er.id
+                                                
+                                                current_pitcher.pitch1 = p_er.pitch1
+                                                if current_pitcher.pitch1 != "None" {
+                                                    current_pitcher.pitch_num += 1
+                                                    current_pitcher.arsenal[0] = p_er.pitch1
+                                                }
+                                                
+                                                current_pitcher.pitch2 = p_er.pitch2
+                                                if current_pitcher.pitch2 != "None" {
+                                                    current_pitcher.pitch_num += 1
+                                                    current_pitcher.arsenal[1] = p_er.pitch2
+                                                }
+                                                
+                                                current_pitcher.pitch3 = p_er.pitch3
+                                                if current_pitcher.pitch3 != "None" {
+                                                    current_pitcher.pitch_num += 1
+                                                    current_pitcher.arsenal[2] = p_er.pitch3
+                                                }
+                                                
+                                                current_pitcher.pitch4 = p_er.pitch4
+                                                if current_pitcher.pitch4 != "None" {
+                                                    current_pitcher.pitch_num += 1
+                                                    current_pitcher.arsenal[3] = p_er.pitch4
+                                                }
+                                                
+                                                fetch_pitches_and_abs(pitcher_id: current_pitcher.idcode)
+                                            }
+                                            
                                         
-                                        if p_er.id != current_pitcher.idcode {
-                                            store_pitcher_appearance()
-                                            clear_game_report()
-                                            current_pitcher.pitch_num = 0
-                                            current_pitcher.firstName = p_er.firstName
-                                            current_pitcher.lastName = p_er.lastName
-                                            current_pitcher.idcode = p_er.id
-                                            selected_pitcher_id = p_er.id
-                                            
-                                            current_pitcher.pitch1 = p_er.pitch1
-                                            if current_pitcher.pitch1 != "None" {
-                                                current_pitcher.pitch_num += 1
-                                                current_pitcher.arsenal[0] = p_er.pitch1
-                                            }
-                                            
-                                            current_pitcher.pitch2 = p_er.pitch2
-                                            if current_pitcher.pitch2 != "None" {
-                                                current_pitcher.pitch_num += 1
-                                                current_pitcher.arsenal[1] = p_er.pitch2
-                                            }
-                                            
-                                            current_pitcher.pitch3 = p_er.pitch3
-                                            if current_pitcher.pitch3 != "None" {
-                                                current_pitcher.pitch_num += 1
-                                                current_pitcher.arsenal[2] = p_er.pitch3
-                                            }
-                                            
-                                            current_pitcher.pitch4 = p_er.pitch4
-                                            if current_pitcher.pitch4 != "None" {
-                                                current_pitcher.pitch_num += 1
-                                                current_pitcher.arsenal[3] = p_er.pitch4
-                                            }
-                                            
-                                            fetch_pitches_and_abs(pitcher_id: current_pitcher.idcode)
-                                        }
+                                        
+//                                        else {
+//                                            
+//                                            showNoReEntry = true
+//                                            
+//                                        }
+                                        
+                                        
+                                        
                                     } label: {
                                         HStack(spacing: 15){
                                             
@@ -214,6 +227,7 @@ struct SelectPitcherView: View {
                     }
                     
                 }
+                
             }
             .onAppear(){
                 if pitchers.count == 0 {
@@ -266,94 +280,28 @@ struct SelectPitcherView: View {
         //Store appearance on change
         //Removes old data if reselected
         
-        for pitcher in scoreboard.pitchers_appearance_list {
-            if pitcher.pitcher_id == current_pitcher.idcode {
-                
-                scoreboard.pitchers_appearance_list = scoreboard.pitchers_appearance_list.filter(){ $0.pitcher_id != current_pitcher.idcode}
-                
-            }
-        }
-        
-        if scoreboard.pitches > 0 {
-            //print("Adding Selected Pitcher")
+        if !scoreboard.pitchers_appearance_list.contains(where: { $0.pitcher_id == current_pitcher.idcode }) && scoreboard.pitches > 0{
+            
             scoreboard.pitchers_appearance_list.append(PitchersAppeared(pitcher_id: current_pitcher.idcode, pitches: scoreboard.pitches, batters_faced: scoreboard.atbats))
+            
         }
         
-        //print("Appearance List:", scoreboard.pitchers_appearance_list)
+//        for pitcher in scoreboard.pitchers_appearance_list {
+//            if pitcher.pitcher_id == current_pitcher.idcode {
+//
+//                scoreboard.pitchers_appearance_list = scoreboard.pitchers_appearance_list.filter(){ $0.pitcher_id != current_pitcher.idcode}
+//
+//            }
+//        }
+        
+//        if scoreboard.pitches > 0 {
+//            //print("Adding Selected Pitcher")
+//
+//        }
+        
+//        print("Appearance List:", scoreboard.pitchers_appearance_list)
         
     }
-    func clear_game_report() {
-        
-        game_report.batters_faced = 0
-        game_report.strikes = 0
-        game_report.balls = 0
-        game_report.hits = 0
-        game_report.strikeouts = 0
-        game_report.walks = 0
-        
-        game_report.first_pitch_strike = 0
-        game_report.first_pitch_ball = 0
-        game_report.first_pit_strike_per = 0
-        game_report.fpb_to_fps = []
-        
-        game_report.strikes_per = 0
-        game_report.balls_to_strikes = []
-        
-        game_report.game_score = 40
-        game_report.pitches = 0
-        
-        game_report.singles = 0
-        game_report.doubles = 0
-        game_report.triples = 0
-        game_report.homeruns = 0
-        game_report.errors = 0
-        game_report.p1_hits = 0
-        game_report.p2_hits = 0
-        game_report.p3_hits = 0
-        game_report.p4_hits = 0
-        game_report.most_hit_pit = ""
-        game_report.mhp_pitches = 0
-        game_report.mhp_hits = 0
-        
-        game_report.swings = 0
-        game_report.swing_per = 0
-        game_report.whiffs = 0
-        game_report.whiff_per = 0
-        
-        game_report.p1_velo_list = []
-        game_report.p2_velo_list = []
-        game_report.p3_velo_list = []
-        game_report.p4_velo_list = []
-        
-        game_report.velo_set_list = []
-        
-        game_report.rh_batters_faced = 0
-        game_report.lh_batters_faced = 0
-        game_report.bs_faced_factor = 0
-        game_report.rh_hits = 0
-        game_report.lh_hits = 0
-        game_report.bs_hits_factor = 0
-        game_report.rh_xbhs = 0
-        game_report.lh_xbhs = 0
-        game_report.bs_xbhs_factor = 0
-        game_report.rh_strikeouts = 0
-        game_report.lh_strikeouts = 0
-        game_report.bs_strikeouts_factor = 0
-        game_report.rh_walks = 0
-        game_report.lh_walks = 0
-        game_report.bs_walks_factor = 0
-        
-        game_report.p1_by_inn = [0]
-        game_report.p2_by_inn = [0]
-        game_report.p3_by_inn = [0]
-        game_report.p4_by_inn = [0]
-        
-        game_report.x_coordinate_list = []
-        game_report.y_coordinate_list = []
-        game_report.pl_color_list = []
-        game_report.pl_outline_list = []
-    }
-    
 }
 
 #Preview {
