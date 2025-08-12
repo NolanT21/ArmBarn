267da70b50bc58f8707c7175d45b9d9cfb690110
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-08-12 12:10:33 -0400

    Added logic for displaying game syncing information in popup

--- ArmBarn/Saved Games/SavedGamesView.swift
+++ ArmBarn/Saved Games/SavedGamesView.swift
@@ -24,9 +24,7 @@ struct SavedGamesView: View {
     @State var showEditGameInfo: Bool = false
     @State var confirmGameDelete: Bool = false
     
-    @State var game_to_delete: SavedGames? = nil
-    
-
+    @State var delete_game_id: UUID = UUID()
     
     @State private var text_color = Color.white
     
@@ -36,6 +34,12 @@ struct SavedGamesView: View {
     @State var show_sync_animation: Bool = false
     @State var sync_animation: Bool = false
     @State var isRotating = 0.0
+    
+    @State var games_downloaded: Int = 0
+    @State var games_uploaded: Int = 0
+    @State var sync_result_image_string: String = ""
+    @State var sync_result_color: Color = .white
+    @State var sync_result_text: String = ""
     
     @State private var engine: CHHapticEngine?
     
@@ -78,23 +82,22 @@ struct SavedGamesView: View {
                                         Button {
                                             
                                             //Logic for calling confirm delete game
-                                            withAnimation{
-                                                confirmGameDelete = true
-                                            }
+                                            confirmGameDelete = true
+                                            delete_game_id = games.game_id
                                             
                                             //    self.itemToDelete = item
                                             
-                                            self.game_to_delete = games
-                                            
-                                            //Logic needs moved to Confirm PopUp
-                                            context.delete(games)
-                                            
-                                            //Logic for deleteing game from server
-                                            if supabaseVM.isAuthenticated == true{
-                                                Task{
-                                                    try await supabaseVM.delete_game(game_id: games.game_id)
-                                                }
-                                            }
+//                                            self.game_to_delete = games
+//                                            
+//                                            //Logic needs moved to Confirm PopUp
+//                                            context.delete(games)
+//                                            
+//                                            //Logic for deleteing game from server
+//                                            if supabaseVM.isAuthenticated == true{
+//                                                Task{
+//                                                    try await supabaseVM.delete_game(game_id: games.game_id)
+//                                                }
+//                                            }
                                             
                                         } label: {
                                             Label("Delete", systemImage: "trash")
@@ -158,104 +161,125 @@ struct SavedGamesView: View {
                 
                 if confirmGameDelete == true {
                     
-                    TwoInputXPopUp(title: "Delete Game", description: "Do you want to delete this game? This game and its data will be permanently deleted", leftButtonText: "Yes", leftButtonAction: {withAnimation{confirmGameDelete = false}}, rightButtonText: "No", rightButtonAction: {withAnimation{confirmGameDelete = false}}, close_action: {withAnimation{confirmGameDelete = false}}, flex_action: {})
+                    TwoInputXPopUp(title: "Delete Game", description: "Do you want to delete this game? This game and its data will be erased", leftButtonText: "Yes", leftButtonAction: {withAnimation{confirmGameDelete = false}; delete_game()}, rightButtonText: "No", rightButtonAction: {withAnimation{confirmGameDelete = false}}, close_action: {withAnimation{confirmGameDelete = false}}, flex_action: {})
                     
                 }
                 
-                SyncGamesPopUp()
+                
+                if show_sync_animation == true{
+                    SyncGamesPopUp()
+                }
                 
             }
         }
         .task{
+            //try await supabaseVM.fetchGames()
             await supabaseVM.isAuthenticated()
+        }
+        .task{
+            do {
+                try await supabaseVM.fetchGames()
+            } catch {
+                print("Error fetching games")
+            }
         }
     }
     
     @ViewBuilder
     func SyncGamesPopUp() -> some View {
         
-        VStack{
-            if show_sync_animation == true{
-                ZStack{
+    VStack{
+            ZStack{
+                
+                Color.black.opacity(0.5)
+                
+                VStack(spacing: 0){
                     
-                    Color.black.opacity(0.5)
+                    HStack{
+                        Text("Syncing Games")
+                            .font(.system(size: 17, weight: .semibold))
+                            .padding(.bottom, 5)
+                    }
+                    
+                    HStack(alignment: .center){
+                        if sync_animation == true {
+                            Text(sync_result_text)
+                                .font(.system(size: 15, weight: .medium))
+                        }
+                    }
+                    .frame(height: 30)
                     
                     VStack{
-                        
-                        HStack{
-                            Text("Syncing Games")
-                        }
-                        
-                        VStack{
-                            if sync_animation == false{
-                                Circle()
-                                    .trim(from: 0.0, to: 0.70)
-                                    .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
-                                    .fill(AngularGradient(gradient: Gradient(colors: [.black, .white]), center: .center, endAngle: .degrees(250)))
-                                    .frame(width: 50, height: 50)
-                                    .rotationEffect(.degrees(isRotating))
-                                    .onAppear {
-                                        withAnimation(.linear(duration: 1).speed(0.9).repeatForever(autoreverses: false)) {
-                                            isRotating = 360
-                                        }
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
-                                            sync_animation = true
-                                            prepareHaptics()
-                                            successHaptic()
-                                        }
+                        if sync_animation == false{
+                            Circle()
+                                .trim(from: 0.0, to: 0.70)
+                                .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
+                                .fill(AngularGradient(gradient: Gradient(colors: [.black, .white]), center: .center, endAngle: .degrees(250)))
+                                .frame(width: 50, height: 50)
+                                .rotationEffect(.degrees(isRotating))
+                                .onAppear {
+                                    withAnimation(.linear(duration: 1).speed(0.9).repeatForever(autoreverses: false)) {
+                                        isRotating = 360
                                     }
-                            }
-                            else {
-                                Image(systemName: "checkmark")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size: 21, weight: .bold))
-                                    .imageScale(.large)
-                                    .bold()
-                                    .onAppear{
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                                        sync_animation = true
+                                        prepareHaptics()
+                                        successHaptic()
+                                    }
+                                }
+                        }
+                        else {
+                            Image(systemName: sync_result_image_string)
+                                .foregroundStyle(sync_result_color)
+                                .font(.system(size: 42, weight: .bold))
+                                .imageScale(.large)
+                                .bold()
+                                .onAppear{
 
-                                        //successHaptic()
-                                        
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                                    //successHaptic()
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                                        withAnimation{
                                             sync_animation = false
-                                            isRotating = 0
                                         }
+                                        isRotating = 0
                                     }
-                            }
+                                }
                         }
-                        .frame(height: 60)
-                        
                     }
-                    .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                    .padding()
-                    .background(Color.black.opacity(0.2))
-                    .background(.thinMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .shadow(radius: 20)
+                    .frame(height: 60)
+                    
                 }
-                .transition(.opacity)
-                .padding(.top, 10)
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                .padding(25)
+                .background(Color.black.opacity(0.2))
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 20)
             }
-                
+            .transition(.opacity)
+            .padding(.top, 10)
         }
-    }
-    
-    func delete_game() {
-        
-//        context.delete(games)
-        
-        //Logic for deleteing game from server
-//        if supabaseVM.isAuthenticated == true{
-//            Task{
-//                try await supabaseVM.delete_game(game_id: games.game_id)
-//            }
-//        }
-        
+        .ignoresSafeArea()
+
     }
     
     func SyncGameData(screenWidth: CGFloat, screenHeight: CGFloat) {
         
+        games_downloaded = 0
+        games_uploaded = 0
+        sync_result_image_string = "checkmark.circle"
+        
         Task{
-            try await supabaseVM.fetchGames()
+            do {
+                try await supabaseVM.fetchGames()
+            } catch {
+                sync_result_image_string = "xmark.circle"
+                sync_result_color = Color.red
+                sync_result_text = "Failed"
+                print("Error syncing game data: \(error)")
+            }
+            
         }
         
         for db_game in supabaseVM.db_games {
@@ -263,7 +287,7 @@ struct SavedGamesView: View {
             var game_stored_locally: Bool = false
             
             for s_game in saved_games {
-                
+
                 if db_game.game_id == s_game.game_id {
                     
                     game_stored_locally = true
@@ -275,14 +299,16 @@ struct SavedGamesView: View {
             print("Stored locally: \(game_stored_locally)")
             if game_stored_locally == false {
                 
+                games_downloaded += 1
+                
                 //Logic for storing game locally
                 let new_saved_game = SavedGames(game_id: db_game.game_id, opponent_name: db_game.opponent, date: db_game.start_time, location: db_game.location, game_data: db_game.game_data, pitcher_info: db_game.pitcher_list)
                 
                 //Need Logic for applying screen dimensions to pitch location data
                 if db_game.screen_width != screenWidth || db_game.screen_height != screenHeight {
                     
-                    var screen_width_factor = screenWidth / db_game.screen_width
-                    var screen_height_factor = screenHeight / db_game.screen_height
+                    let screen_width_factor = screenWidth / db_game.screen_width
+                    let screen_height_factor = screenHeight / db_game.screen_height
                     
                     for (index, event) in new_saved_game.game_data.enumerated() {
                         new_saved_game.game_data[index].pitch_x_location *= screen_width_factor
@@ -311,17 +337,39 @@ struct SavedGamesView: View {
             }
             
             print("Stored in DB: \(game_stored_in_db)")
+            
             if game_stored_in_db == false {
+                
+                games_uploaded += 1
                 
                 //Logic for storing game in database
                 Task{
-                    try await supabaseVM.insertGame(game_id: s_game.game_id, opponent: s_game.opponent_name, location: s_game.location, startTime: s_game.date, event_list: [], pitcher_list: s_game.pitcher_info, screenWidth: screenWidth, screenHeight: screenHeight)
+                    do {
+                        try await supabaseVM.insertGame(game_id: s_game.game_id, opponent: s_game.opponent_name, location: s_game.location, startTime: s_game.date, event_list: s_game.game_data, pitcher_list: s_game.pitcher_info, screenWidth: screenWidth, screenHeight: screenHeight)
+                    } catch {
+                        sync_result_image_string = "xmark.circle"
+                        sync_result_color = Color.red
+                        sync_result_text = "Failed"
+                        print("Error syncing game data: \(error)")
+                    }
+                    
                 }
             }
             
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        if games_downloaded == 0 && games_uploaded == 0 {
+            sync_result_text = "No data to sync"
+            sync_result_color = Color.yellow
+            sync_result_image_string = "minus.circle"
+        }
+        else if games_downloaded > 0 || games_uploaded > 0 {
+            sync_result_text = "\(games_downloaded) game(s) downloaded, \(games_uploaded) game(s) uploaded"
+            sync_result_color = Color.green
+            sync_result_image_string = "minus.circle"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
             withAnimation{
                 show_sync_animation.toggle()
             }
@@ -329,10 +377,27 @@ struct SavedGamesView: View {
         
     }
     
-    func removeSavedGame(at indexSet: IndexSet) {
-        for index in indexSet {
-            context.delete(saved_games[index])
+    func delete_game() {
+        
+        for game in saved_games {
+            
+            if game.game_id == delete_game_id {
+                
+                context.delete(game)
+                
+                //Logic for deleteing game from server
+                if supabaseVM.isAuthenticated == true{
+                    Task{
+                        try await supabaseVM.delete_game(game_id: game.game_id)
+                    }
+                }
+                
+                break
+                
+            }
+            
         }
+
     }
     
     func successHaptic() {
